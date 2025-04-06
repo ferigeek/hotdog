52021e9b63d957eca71de83595c3d30a358a7bb8
ferigeek <150931210+ferigeek@users.noreply.github.com>
2025-04-06 21:25:26 +0330

    Added some models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class HotdogUser(AbstractUser):
+    username = models.IntegerField(unique=True, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    field_of_study = models.CharField(max_length=255, blank=True, null=True)
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'field_of_study']
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=255, blank=True, null=True)
+    instructor = models.CharField(max_length=255, blank=True, null=True)
+    field_of_study = models.CharField(max_length=255, blank=True, null=True)
+    semester = models.CharField(max_length=50, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    def clean_year(self):
+        pass
+
+    def __str__(self):
+        return f"{self.course_name} - {self.semester} {self.year}"
--- task/models.py
+++ task/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from core.models import HotdogUser, Course
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(auto_now_add=True)
+    notify_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
+    created_by = models.ForeignKey(HotdogUser, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.title} - {self.deadline}"
+    
+class TaskHistory(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    moddified_by = models.ForeignKey(HotdogUser, on_delete=models.CASCADE, related_name='task_history')
+    pre_title = models.CharField(max_length=200, blank=True)
+    pre_description = models.TextField(blank=True)
+    pre_created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    pre_deadline = models.DateTimeField(auto_now_add=True, blank=True)
+    pre_notify_at = models.DateTimeField(auto_now_add=True, blank=True)
+    pre_completed = models.BooleanField(default=False, blank=True)
